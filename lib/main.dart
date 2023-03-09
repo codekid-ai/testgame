bc3b316a7e7526c4e716380bd7a545b92d228ff2
kovarny <sivochkin@gmail.com>
2023-03-10 09:50:09 +1100

    deploy

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:isolate';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -10,8 +11,23 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'firebase_options.dart';
 import 'interop.dart';
 
+// import 'package:flutter/services.dart';
+
+// const platform = MethodChannel('com.example.myapp/mychannel');
+
+// void _handleMethodCall(MethodCall call) {
+//   if (call.method == 'mychannel') {
+//     context.read(myProvider).state = "Updated Value";
+//   }
+// }
+
+// final container = ProviderContainer();
+
 void saySomething(String arg) {
   print('Dart function called with argument : $arg');
+  // container.read(myProvider.notifier).state = arg;
+  // container.refresh(myProvider);
+  _streamController.add('say $arg');
 }
 
 void main() async {
@@ -20,26 +36,36 @@ void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
 
-  FirebaseFirestore.instance
-      .collection('project/testgame/code')
-      .snapshots()
-      .listen((codes) => codes.docs.forEach((e) {
-            // print(e.data());
-            registerEvent(e.data()['handler'],
-                "try { \n ${e.data()['code']} \n } catch (e) { \n logError('${e.data()['handler']}', e); \n }");
-          }));
+  // FirebaseFirestore.instance
+  //     .collection('project/testgame/code')
+  //     .snapshots()
+  //     .listen((codes) => codes.docs.forEach((e) {
+  //           // print(e.data());
+  //           registerEvent(e.data()['handler'],
+  //               "try { \n ${e.data()['code']} \n } catch (e) { \n logError('${e.data()['handler']}', e); \n }");
+  //         }));
 
-  Interop.initLog(
-      FirebaseFirestore.instance.collection('project/testgame/output'));
+  // Interop.initLog(
+  //     FirebaseFirestore.instance.collection('project/testgame/output'));
 
   runApp(ProviderScope(child: MainApp()));
 }
 
-final something = StateProvider((ref) => 'hello');
+//final something = StateProvider((ref) => 'hello');
+
+// final myProvider = Provider((_) => "Hello, World!");
+//final myProvider = StateProvider((_) => "Hello, World!");
+
+StreamController<String> _streamController = StreamController<String>();
+Stream<String> get stream => _streamController.stream;
+
+void sendFunctionCall(String functionCall) {
+  _streamController.add(functionCall);
+}
 
 class MainApp extends ConsumerWidget {
   const MainApp({
@@ -48,23 +74,27 @@ class MainApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // final value = ref.watch(myProvider);
+
+    //container.dispose();
     //bool isDarkTheme = ref.watch(themeStateNotifierProvider);
     return MaterialApp(
-        title: 'Test Game',
+        title: 'Test Game 1',
         // themeMode: isDarkTheme ? ThemeMode.dark : ThemeMode.light,
-        home:
-            Scaffold(body: Text('say: ${ref.watch(something.notifier).state}'))
-        //TheApp(),
-        // SandboxLauncher(
-        //   app: TheApp(),
-        //   sandbox: Sandbox(),
-        //   getInitialState: () async {
-        //     return (await kDB.doc('sandbox/serge').get()).data();
-        //   },
-        //   saveState: (s) {
-        //     kDB.doc('sandbox/serge').set({'sandbox': s});
-        //   },
-        // )
-        );
+        home: Scaffold(
+            body: StreamBuilder<String>(
+          stream: _streamController.stream,
+          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+            if (snapshot.hasData) {
+              // Do something with the function call data
+              return Text(snapshot.data!);
+            } else {
+              return Text('Waiting for function calls...');
+            }
+          },
+        )
+            //Text('say')
+            // Text('say  : ${value}')
+            ));
   }
 }
