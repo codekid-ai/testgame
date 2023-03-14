1db6cca6de7bad58027f1e44b0e4ab7283ebcd18
kovarny <sivochkin@gmail.com>
2023-03-14 16:03:39 +1100

    interop example

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # testgame
+
+This game demostrates the interop framework of codeKid.
+
+To understand how it works:
+1. Launch the app in Chrome using VSCode
+2. Open browser console (Inspector)
+3. Write 'myDartFunction('hello world')' in console and execute
+
+You should see the message: 'say hello world' on the screen.
+
+The javascript function was called and the message was received in Flutter app (the game) to be printed on the screen.
+
+The games will be created in Flutter.
+But kids should be able to program some functionality (that is exposed by the game publisher) using javascript.
--- lib/main.dart
+++ lib/main.dart
@@ -7,26 +7,13 @@ import 'package:flutter/material.dart';
 import 'dart:js';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'firebase_options.dart';
 import 'interop.dart';
 
-// import 'package:flutter/services.dart';
-
-// const platform = MethodChannel('com.example.myapp/mychannel');
-
-// void _handleMethodCall(MethodCall call) {
-//   if (call.method == 'mychannel') {
-//     context.read(myProvider).state = "Updated Value";
-//   }
-// }
-
-// final container = ProviderContainer();
+StreamController<String> _streamController = StreamController<String>();
 
 void saySomething(String arg) {
   print('Dart function called with argument : $arg');
-  // container.read(myProvider.notifier).state = arg;
-  // container.refresh(myProvider);
   _streamController.add('say $arg');
 }
 
@@ -36,35 +23,7 @@ void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
 
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
-
-  // FirebaseFirestore.instance
-  //     .collection('project/testgame/code')
-  //     .snapshots()
-  //     .listen((codes) => codes.docs.forEach((e) {
-  //           // print(e.data());
-  //           registerEvent(e.data()['handler'],
-  //               "try { \n ${e.data()['code']} \n } catch (e) { \n logError('${e.data()['handler']}', e); \n }");
-  //         }));
-
-  // Interop.initLog(
-  //     FirebaseFirestore.instance.collection('project/testgame/output'));
-
   runApp(ProviderScope(child: MainApp()));
-}
-
-//final something = StateProvider((ref) => 'hello');
-
-// final myProvider = Provider((_) => "Hello, World!");
-//final myProvider = StateProvider((_) => "Hello, World!");
-
-StreamController<String> _streamController = StreamController<String>();
-Stream<String> get stream => _streamController.stream;
-
-void sendFunctionCall(String functionCall) {
-  _streamController.add(functionCall);
 }
 
 class MainApp extends ConsumerWidget {
@@ -74,13 +33,8 @@ class MainApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // final value = ref.watch(myProvider);
-
-    //container.dispose();
-    //bool isDarkTheme = ref.watch(themeStateNotifierProvider);
     return MaterialApp(
         title: 'Test Game 1',
-        // themeMode: isDarkTheme ? ThemeMode.dark : ThemeMode.light,
         home: Scaffold(
             body: StreamBuilder<String>(
           stream: _streamController.stream,
@@ -92,9 +46,6 @@ class MainApp extends ConsumerWidget {
               return Text('Waiting for function calls...');
             }
           },
-        )
-            //Text('say')
-            // Text('say  : ${value}')
-            ));
+        )));
   }
 }
